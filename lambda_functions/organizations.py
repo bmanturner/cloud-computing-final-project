7295f8f7ec2a54d839b7a1dc3ee6471cf57c9504
Brendan Turner <bfturner@foglet.io>
2020-06-29 17:15:36 -0500

    add basic api_key check for create functions

--- lambda_functions/organizations.py
+++ lambda_functions/organizations.py
@@ -11,12 +11,27 @@ db = pymysql.connect(host=os.environ["DB_HOST"], user=os.environ["DB_USERNAME"],
 
 ## HELPERS
 
+# convert datetime to ISO string
 def mysql_converter(o):
     if isinstance(o, datetime.datetime):
         return o.__str__()
 
+# read api_key from request and ensure user exists and is an admin
+def authenticate_api_key(event):
+    if "api_key" not in event:
+        raise Exception("An api_key is required to continue")
+    with db.cursor() as cursor:
+        cursor.execute("SELECT * FROM Users WHERE api_key = %s;", (event["api_key"], ))
+        user = cursor.fetchone()
+        if user is None:
+            raise Exception("No user has this api_key")
+        if user["role"] != "admin":
+            raise Exception("This user is not an admin")
+        return user
+
 ## FUNCTIONS
 
+# returns list of organizations
 def get_organizations(event, context):
     with db.cursor() as cursor:
         cursor.execute("SELECT * FROM Organizations;")
@@ -26,6 +41,14 @@ def get_organizations(event, context):
         return result
 
 def create_organization(event, context):
+    result = {}
+    try:
+        authenticate_api_key(event)
+    except Exception as e:
+        result["status"] = 401
+        result["body"] = repr(e)
+        return result
+
     name = event["name"]
     s3_prefix = event["s3_prefix"].lower()
     result = {}
@@ -34,11 +57,18 @@ def create_organization(event, context):
         result["body"] = "s3_prefix can not contain spaces"
         return result
 
-    with db.cursor() as cursor:
-        sql = "INSERT INTO Organizations (name, s3_prefix) VALUES (%s, %s);"
-        cursor.execute(sql, (name, s3_prefix))
-    db.commit()
+    # create
+    try:
+        with db.cursor() as cursor:
+            sql = "INSERT INTO Organizations (name, s3_prefix) VALUES (%s, %s);"
+            cursor.execute(sql, (name, s3_prefix))
+        db.commit()
+    except Exception as e:
+        result["status"] = 500
+        result["body"] = repr(e)
+        return result
 
+    # return created organization
     with db.cursor() as cursor:
         sql = "SELECT * FROM Organizations WHERE name = %s"
         cursor.execute(sql, (name, ))
@@ -47,9 +77,9 @@ def create_organization(event, context):
         result["body"] = json.dumps(cursor.fetchone(), default=mysql_converter)
         return result
 
-
 if __name__ == "__main__":
     req = {}
     req["name"] = "Alpha Dynamics"
     req["s3_prefix"] = "alpha-dynamics"
+    req["api_key"] = "651b5ca0-c568-4cb2-9e33-ac9ac4d3d224"
     print(create_organization(req, ''))
--- lambda_functions/users.py
+++ lambda_functions/users.py
@@ -11,12 +11,27 @@ db = pymysql.connect(host=os.environ["DB_HOST"], user=os.environ["DB_USERNAME"],
 
 ## HELPERS
 
+# convert datetime to ISO string
 def mysql_converter(o):
     if isinstance(o, datetime.datetime):
         return o.__str__()
 
+# read api_key from request and ensure user exists and is an admin
+def authenticate_api_key(event):
+    if "api_key" not in event:
+        raise Exception("An api_key is required to continue")
+    with db.cursor() as cursor:
+        cursor.execute("SELECT * FROM Users WHERE api_key = %s;", (event["api_key"], ))
+        user = cursor.fetchone()
+        if user is None:
+            raise Exception("No user has this api_key")
+        if user["role"] != "admin":
+            raise Exception("This user is not an admin")
+        return user
+
 ## FUNCTIONS
 
+# returns list of all users
 def get_users(event, context):
     with db.cursor() as cursor:
         cursor.execute("SELECT * FROM Users;")
@@ -25,21 +40,42 @@ def get_users(event, context):
         result["body"] = json.dumps(cursor.fetchall(), default=mysql_converter)
         return result
 
+def get_organization_users(event, context):
+    org_id = event["org_id"]
+    with db.cursor() as cursor:
+        cursor.execute("SELECT * FROM Users WHERE org_id = %s", (org_id, ))
+        result = {}
+        result["status"] = 200
+        result["body"] = json.dumps(cursor.fetchall(), default=mysql_converter)
+        return result
+
 def register_user(event, context):
+    result = {}
+    try:
+        authenticate_api_key(event)
+    except Exception as e:
+        result["status"] = 401
+        result["body"] = repr(e)
+        return result
+
     email = event["email"]
     username = event["username"]
     password = event["password"]
     org_id = event["org_id"]
 
-    with db.cursor() as cursor:
-        sql = "INSERT INTO Users (email, username, password, api_key, org_id, updated_at, created_at) VALUES (%s, %s, %s, %s, %s, NOW(), NOW());"
-        cursor.execute(sql, (email, username, password, str(uuid4()), org_id))
-    db.commit()
+    try:
+        with db.cursor() as cursor:
+            sql = "INSERT INTO Users (email, username, password, api_key, org_id, updated_at, created_at) VALUES (%s, %s, %s, %s, %s, NOW(), NOW());"
+            cursor.execute(sql, (email, username, password, str(uuid4()), org_id))
+        db.commit()
+    except Exception as e:
+        result["status"] = 500
+        result["body"] = repr(e)
+        return result
 
     with db.cursor() as cursor:
         sql = "SELECT * FROM Users WHERE email = %s"
         cursor.execute(sql, (email, ))
-        result = {}
         result["status"] = 200
         result["body"] = json.dumps(cursor.fetchone(), default=mysql_converter)
         return result
@@ -71,6 +107,5 @@ def login(event, context):
 
 if __name__ == "__main__":
     req = {}
-    req["username"] = "fakeusername1"
-    req["password"] = "fakepassword1"
-    print(login(req, ''))
+    req["api_key"] = 3
+    print(register_user(req, ''))
